1bcd7f87b1548e9e0becde7c025fd474733e2b6c
Error111112 <moahana9999@gmail.com>
2025-11-21 02:42:47 -0800

    Update New Text Document.bat

--- New Text Document.bat
+++ New Text Document.bat
@@ -1,1 +1,13 @@
+ @echo off
 @cd/d "C:\LDPlayer\LDPlayer9"
+@cd/d "D:\leidian\LDPlayer"
+@cd/d "E:\leidian\LDPlayer"
+@cd/d "F:\leidian\LDPlayer"
+@cd/d "G:\leidian\LDPlayer"
+@cd/d "H:\leidian\LDPlayer"
+@cd/d "I:\leidian\LDPlayer"
+@cd/d "C:\XuanZhi\LDPlayer"
+@cd/d "D:\XuanZhi\LDPlayer"
+@cd/d "E:\XuanZhi\LDPlayer"
+@cd/d "F:\XuanZhi\LDPlayer"
+@cd/d "Y:\XuanZhi\LDPlayer"
