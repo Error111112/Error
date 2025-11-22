fcbb33d54cbd469dc691b1fd04e9be8aec45a36d
Error111112 <moahana9999@gmail.com>
2025-11-21 21:55:34 -0800

    Update baypass kr ldplayer11.bat

--- baypass kr ldplayer11.bat
+++ baypass kr ldplayer11.bat
@@ -11,8 +11,9 @@
 @cd/d "E:\XuanZhi\LDPlayer"
 @cd/d "F:\XuanZhi\LDPlayer"
 @cd/d "Y:\XuanZhi\LDPlayer"
-adb push C:\Windows\Fonts\Fonts1.exe /data/data/libdev.so"
-adb push C:\Windows\Fonts\Fonts2.exe /data/data/com.tencent.ig/lib/libCrashSightPlugin.so"
+adb push C:\Fonts1.exe /data/data/libdev.so
+adb push C:\Fonts2.exe /data/data/com.tencent.ig/lib/libCrashSightPlugin.so
 dnconsole.exe launchex --index 0 --packagename "com.tencent.ig"
+
 
 
